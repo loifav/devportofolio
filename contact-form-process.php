2bd1577aa649eb7a4ece97a06d424a2a08bf66ea
loifav <120129600+loifav@users.noreply.github.com>
2022-12-09 20:51:30 +0100

    Add files via upload
    test

---
+++ contact-form-process.php
@@ -0,0 +1,23 @@
+<?php
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "favrelc22@gmail.com"; 
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
+?>
